db8963f5459ec780ebb5497bf9a97665f797bf86
Luis Papiernik <lpapiernik24@gmail.com>
2022-07-28 19:11:09 -0500

    format some project files

--- image2ascii/utils.py
+++ image2ascii/utils.py
@@ -7,7 +7,7 @@ AsciiImage = List[List[str]]
 
 def get_gray_image(filename: str) -> Image.Image:
     img = Image.open(filename)
-    gray = img.convert('L')
+    gray = img.convert("L")
     img.close()
 
     return gray
@@ -17,13 +17,10 @@ def image_to_ascii(image: Image.Image, ascii_color_palette: str) -> AsciiImage:
     pixels = image.getdata()
 
     groups_number = 256 / len(ascii_color_palette)
-    char_data = [
-        ascii_color_palette[int(pixel / groups_number)]
-        for pixel in pixels
-    ]
+    char_data = [ascii_color_palette[int(pixel / groups_number)] for pixel in pixels]
 
     ascii_image = [
-        char_data[index: index + image.width]
+        char_data[index : index + image.width]
         for index in range(0, len(char_data), image.width)
     ]
 
--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from argparse import ArgumentParser
 
 from image2ascii import convert_image
 
+DESCRIPTION = """This program converts an image to
+ASCII and saves it in png and txt files"""
 
-DESCRIPTION = '''This program converts an image to
-ASCII and saves it in png and txt files'''
-
-FORMAT_HELP = '''PNG tells the program that the output is an
+FORMAT_HELP = """PNG tells the program that the output is an
 image, TXT indicates the output is in plain text and
-and BOTH indicates both formats'''
+and BOTH indicates both formats"""
 
-INVERT_HELP = '''invert output image. Use if your display has
-a dark background'''
+INVERT_HELP = """invert output image. Use if your display has
+a dark background"""
 
 
 def execute(args):
@@ -22,24 +21,26 @@ def main() -> None:
     parser = ArgumentParser(description=DESCRIPTION)
 
     parser.add_argument(
-        '-i', '--input', type=str, metavar='INPUT',
-        help='name of the input image'
+        "-i", "--input", type=str, metavar="INPUT", help="name of the input image"
     )
     parser.add_argument(
-        '-o', '--output', default='ascii', type=str,
-        help='names of the output file without format'
+        "-o",
+        "--output",
+        default="ascii",
+        type=str,
+        help="names of the output file without format",
     )
     parser.add_argument(
-        '--format', default='PNG', metavar='PNG/TXT/BOTH',
-        choices=['PNG', 'TXT', 'BOTH'], help=FORMAT_HELP,
+        "--format",
+        default="PNG",
+        metavar="PNG/TXT/BOTH",
+        choices=["PNG", "TXT", "BOTH"],
+        help=FORMAT_HELP,
     )
     parser.add_argument(
-        '-f', '--font', default='', type=str,
-        help='font to be used for the conversion'
+        "-f", "--font", default="", type=str, help="font to be used for the conversion"
     )
-    parser.add_argument(
-        '--invert', action='store_true', help=INVERT_HELP
-    )
+    parser.add_argument("--invert", action="store_true", help=INVERT_HELP)
 
     args = parser.parse_args()
     execute(args)
